20c451f95272dbc3b9d3a9ced4681257c2fbf252
lrncrd <lorenzocardarelli2@gmail.com>
2024-06-19 17:00:47 +0200

    first push

---
+++ utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def find_bounding_box(mask):
+    rows = np.any(mask, axis=1)
+    cols = np.any(mask, axis=0)
+    rmin, rmax = np.where(rows)[0][[0, -1]]
+    cmin, cmax = np.where(cols)[0][[0, -1]]
+    return rmin, rmax, cmin, cmax
